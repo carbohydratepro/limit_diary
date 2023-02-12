08262d74f5af77f6d3370a6d8a331d32fcc1262b
carbohydratepro <carbohydratepro@gmail.com>
2023-02-13 01:02:46 +0900

    viewにいいねを実装

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -1,4 +1,8 @@
 class Blog < ApplicationRecord
   belongs_to :user
   has_many :favorites, dependent: :destroy
+
+  def favorited_by?(user)
+    favorites.where(user_id: user.id).exists?
+  end
 end
