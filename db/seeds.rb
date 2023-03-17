28fe4a4aa9f7813fd41b7601d6152263951089b4
carbohydratepro <carbohydratepro@gmail.com>
2023-03-18 06:26:05 +0900

    userのseedデータを生成するpythonファイルを追加

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,5 @@
-60.times do |n|
-  Blog.create!(
-    title: Faker::Book.title,
-    body: "初の投稿です。よろしくお願いします！",
-    user_id: 1,
-  )
+require "csv"
+
+CSV.foreach('db/users.csv').with_index do |row, index|
+  User.create(:name => row[0], :password => row[1], :email => row[2], :introduction => row[3], :created_at => row[4], :updated_at => row[5], :image => File.open("./app/assets/images/icon_#{index}.png"))
 end
